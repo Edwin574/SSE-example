09196933f77aaf91ee0439a18242f26f0da78ae8
“Edwin574” <owinoedwin574@gmail.com>
2025-06-05 00:15:43 +0300

    Making the events truly async in asgi.py

--- sim/urls.py
+++ sim/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-    path("stream-data/", views.sse_stream, name='stream_data'),
 ]
--- sse/asgi.py
+++ sse/asgi.py
@@ -1,16 +1,35 @@
-"""
-ASGI config for sse project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
+import asyncio
 from django.core.asgi import get_asgi_application
-
+import random
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sse.settings')
+django_app = get_asgi_application()
 
-application = get_asgi_application()
+async def sse_stream(scope, receive, send):
+    assert scope["type"] == "http"
+    headers = [(b"content-type", b"text/event-stream")]
+    await send({
+        "type": "http.response.start",
+        "status": 200,
+        "headers": headers,
+    })
+    emojis = ["🚀", "🐎", "🌅", "🦾", "🍇"]
+    i = 0
+    try:
+        while True:
+            data = f"data: {random.choice(emojis)} {i}\n\n"
+            await send({
+                "type": "http.response.body",
+                "body": data.encode("utf-8"),
+                "more_body": True,
+            })
+            await asyncio.sleep(1)
+            i += 1
+    except asyncio.CancelledError:
+        print("SSE client disconnected")
+
+async def application(scope, receive, send):
+    if scope["type"] == "http" and scope["path"] == "/stream-data/":
+        await sse_stream(scope, receive, send)
+    else:
+        await django_app(scope, receive, send)
